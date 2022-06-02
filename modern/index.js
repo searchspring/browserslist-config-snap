1060780eca76aa890504e33f96ea21c71fb4da27
kevin <kevin@searchspring.com>
2022-06-02 16:49:51 -0600

    updating modern browsers list

--- modern/index.js
+++ modern/index.js
@@ -4,9 +4,8 @@ module.exports = [
 	"Edge >= 91",
 	"Firefox >= 74",
 	"FirefoxAndroid >= 74",
-	"Safari >= 13.1",
-	"ios_saf 13.4",
-	"iOS >= 13.4",
+	"Safari >= 14",
+	"iOS >= 14",
 	"Opera >= 77",
 	"Android >= 91"
 ]
